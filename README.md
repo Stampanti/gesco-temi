87fb1cfdeda19f66855e4c44199deb8e2a3226df
Cutotopo <cuttinif@gmail.com>
2022-02-21 17:40:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Themes for Gesco
+
+CSS themes for Gesco. Use with an extension like [Stylus](https://github.com/openstyles/stylus/), or with our theme loader Userscript.
+
+## Themes
+| Theme | Type | Files |
+|-------|------|----------|
+| Dark  | Full | [theme.css](themes/dark/theme.css) |
+| High contrast calendar | Calendar| [theme.css](themes/calendar-high-contrast/theme.css) |
---
+++ themes/thememanager.js
@@ -0,0 +1,40 @@
+var themes = ["https://gs-themes.videostampantimc.ga/themes/dark/theme.css", "https://gs-themes.videostampantimc.ga/themes/calendar-high-contrast/theme.css"];
+var themesname = ["dark", "hc"];
+var themes_rel = "stylesheet";
+
+function toggleThemeListItem(themeid) {
+    $("#" + themeid + "ThemeListItem").toggleClass("active");
+    $(document.body).toggleClass("gs-" + themeid);
+    if (window.localStorage.getItem("gs-" + themeid) == "true") {
+        window.localStorage.setItem("gs-" + themeid, "false");
+    } else {
+        window.localStorage.setItem("gs-" + themeid, "true");
+    }
+}
+
+var themeManager = `
+<li class="">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+<i class="fas fa-brush"></i> Tema <span class="caret"></span>
+</a>
+<ul class="dropdown-menu" role="menu">
+<li onclick="toggleThemeListItem('dark')" id="darkThemeListItem"><a><i class="fas fa-brush"></i>Tema scuro <span class="badge">BETA</span></a></li>
+<li onclick="toggleThemeListItem('hc')" id="hcThemeListItem"><a><i class="fas fa-brush"></i> Tema a contrasto elevato</a></li>
+</ul>
+</li>
+`;
+
+document.addEventListener('DOMContentLoaded', function() {
+    for (var i = 0; i < themes.length; i++) {
+        var el = document.createElement("link");
+        el.href = themes[i];
+        el.rel = themes_rel;
+        document.head.appendChild(el);
+    }
+    for (var j = 0; j < themes.length; j++) {
+        if (window.localStorage.getItem("gs-" + themesname[j]) == "true"){
+            toggleThemeListItem(themesname[j])
+        }
+    }
+    $(".navbar-right").prepend($(themeManager));
+});
