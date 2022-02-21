ff2766b088537afc8bd1e3307f5f367970f87337
Cutotopo <cuttinif@gmail.com>
2022-02-21 18:50:03 +0100

    Part 2 of previous commit :)

--- themes/thememanager.js
+++ themes/thememanager.js
@@ -2,10 +2,10 @@ var themes = ["https://gs-themes.videostampantimc.ga/themes/dark/theme.css", "ht
 var themesname = ["dark", "hc"];
 var themes_rel = "stylesheet";
 
-function toggleThemeListItem(themeid) {
+function toggleThemeListItem(themeid, s) {
     $("#" + themeid + "ThemeListItem").toggleClass("active");
     $(document.body).toggleClass("gs-" + themeid);
-    if (window.localStorage.getItem("gs-" + themeid) == "true") {
+    if (window.localStorage.getItem("gs-" + themeid) == "true" && s == 1) {
         window.localStorage.setItem("gs-" + themeid, "false");
     } else {
         window.localStorage.setItem("gs-" + themeid, "true");
@@ -18,8 +18,8 @@ var themeManager = `
 <i class="fas fa-brush"></i> Tema <span class="caret"></span>
 </a>
 <ul class="dropdown-menu" role="menu">
-<li onclick="toggleThemeListItem('dark')" id="darkThemeListItem"><a><i class="fas fa-brush"></i> Tema scuro <span class="badge">BETA</span></a></li>
-<li onclick="toggleThemeListItem('hc')" id="hcThemeListItem"><a><i class="fas fa-brush"></i> Tema a contrasto elevato</a></li>
+<li onclick="toggleThemeListItem('dark', 1)" id="darkThemeListItem"><a><i class="fas fa-brush"></i> Tema scuro <span class="badge">BETA</span></a></li>
+<li onclick="toggleThemeListItem('hc', 1)" id="hcThemeListItem"><a><i class="fas fa-brush"></i> Tema a contrasto elevato</a></li>
 </ul>
 </li>
 `;
@@ -31,9 +31,9 @@ for (var i = 0; i < themes.length; i++) {
     document.head.appendChild(el);
 }
 if (window.localStorage.getItem("gs-dark") == "true"){
-    toggleThemeListItem("dark")
+    toggleThemeListItem("dark", 0)
 }
 if (window.localStorage.getItem("gs-hc") == "true"){
-    toggleThemeListItem("hc")
+    toggleThemeListItem("hc", 0)
 }
 $(".navbar-right").prepend($(themeManager));
